2cbe684eec29494571bec592a48ae212ae8154e5
IAmShaaann <iamshaaann@gmail.com>
2025-06-10 22:17:52 +0530

    Enhance configuration and layout components
    - Updated Next.js configuration to enable SWC minification and adjust production settings.
    - Replaced Geist font with Poppins for improved typography in layout components.
    - Added DashboardNavbar to the dashboard layout for better navigation.
    - Enhanced DashboardUserButton with a responsive drawer for mobile users, integrating user session information and logout functionality.

--- makai/src/app/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ makai/src/app/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,17 +1,20 @@
 import { authClient } from "@/lib/auth-client";
+import { ChevronDown, CreditCardIcon, LogOutIcon } from "lucide-react";
+
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuLabel, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger} from "@/components/ui/drawer";
+
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
-import { ChevronDown, CreditCardIcon, LogOutIcon } from "lucide-react";
+
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
   const router = useRouter();
+  const isMobile = useIsMobile();
   const { data: session , isPending} = authClient.useSession();
-
-  if(!session?.user) return null;
-  if(isPending) return null;
-  console.log("session: ", session.user.image)
 
   const onLogout = () => {
     authClient.signOut({
@@ -20,9 +23,50 @@ export const DashboardUserButton = () => {
       }
     })
   }
+
+  if(!session?.user) return null;
+  if(isPending) return null;
+
+  if(isMobile){
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 gap-x-2 overflow-hidden">
+          {session.user.image ? (
+            <Avatar>
+              <AvatarImage src={session.user.image}/>
+            </Avatar>
+          ) : 
+          <GeneratedAvatar variant="initials" className="size-9 mr-3" seed={session.user.name}/>}
+          <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+            <p className="text-sm truncate w-full">{session.user.name}</p>
+            {/* <p className="text-xs truncate w-full">{session.user.email}</p> */}
+          </div>
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>
+              {session.user.name}
+            </DrawerTitle>
+            <DrawerDescription>
+              {session.user.email}
+            </DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant={"outline"} onClick={()=> {}}>
+              Billing <CreditCardIcon className="size-4 text-black"/>
+            </Button>
+            <Button variant={"outline"} onClick={onLogout}>
+              Logout <LogOutIcon className="size-4"/>
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+
   
   return <DropdownMenu>
-    <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+    <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 gap-x-2 overflow-hidden">
       {session.user.image ? (
         <Avatar>
           <AvatarImage src={session.user.image}/>
