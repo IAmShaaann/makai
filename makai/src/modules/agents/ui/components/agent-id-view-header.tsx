f9a3921133d61201b2cacb21bdd9e79412c519bc
IAmShaaann <=>
2025-06-12 20:59:41 +0530

    Implement agent detail view and enhance agent retrieval logic; add error handling and loading states

---
+++ makai/src/modules/agents/ui/components/agent-id-view-header.tsx
@@ -0,0 +1,55 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuContent } from "@/components/ui/dropdown-menu"
+import { ChevronRightIcon, MoreHorizontalIcon, PencilIcon, TrashIcon } from "lucide-react";
+import Link from "next/link";
+
+interface AgentIdViewProps {
+  agentId: string;
+  agentName: string;
+  onEdit: () => void;
+  onRemove: () => void;
+}
+export const AgentIdViewHeader = ({ agentId, agentName, onEdit, onRemove }: AgentIdViewProps) => {
+  return <div className="flex items-center justify-between">
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild className="font-medium text-xl">
+            <Link href={"/agents"}>
+              Available Agents
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
+          <ChevronRightIcon />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild className="font-medium text-xl text-muted-foreground">
+            <Link href={`/agents/${agentId}`}>
+              {agentName}
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"}>
+          <MoreHorizontalIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={onEdit}>
+          <PencilIcon className="size-4 text-black" />
+          Edit
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={onRemove}>
+          <TrashIcon className="size-4 text-black" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+
+    </DropdownMenu>
+  </div>
+}
