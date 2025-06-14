cd8e9c8ebf46d8a36f86b136d3fc7e27b8b01a4d
IAmShaaann <=>
2025-06-14 23:39:49 +0530

    Enhance meetings page with authentication and UI improvements; add CRUD operations for meetings
    - Implemented session validation to redirect unauthenticated users to the sign-in page.
    - Added MeetingsListHeader component to the meetings page for better UI organization.
    - Introduced CRUD operations for meetings in the server procedures, including create, update, and remove functionalities.
    - Cleaned up MeetingsView by removing unnecessary data display.

---
+++ makai/src/components/command-select.tsx
@@ -0,0 +1,82 @@
+"use client";
+import {
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandResponsiveDialog,
+  CommandEmpty,
+} from "@/components/ui/command";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { ChevronsUpDownIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface CommandSelectProps {
+  options: Array<{
+    id: string;
+    value: string;
+    children?: React.ReactNode;
+  }>;
+  onSelect: (value: string) => void;
+  onSearch?: (query: string) => void;
+  value: string;
+  isSearchable?: boolean;
+  className?: string;
+  placeholder?: string;
+}
+
+export const CommandSelect = ({
+  options,
+  onSelect,
+  onSearch,
+  value,
+  isSearchable,
+  className,
+  placeholder = "Select an option",
+}: CommandSelectProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const selectedOption = options.find((option) => option.value === value);
+
+  return (
+    <>
+      <Button
+        onClick={() => setOpen(true)}
+        type="button"
+        variant={"outline"}
+        className={cn(
+          "h-9 justify-between px-4 font-normal",
+          !selectedOption && "text-muted-foreground",
+          className,
+        )}
+      >
+        <div>{selectedOption?.children ?? placeholder}</div>
+        <ChevronsUpDownIcon />
+      </Button>
+      <CommandResponsiveDialog
+        open={open}
+        onOpenChange={setOpen}
+        shouldFilter={!onSearch}
+      >
+        <CommandInput placeholder="Search..." onValueChange={onSearch} />
+        <CommandList>
+          <CommandEmpty>
+            <span className="text-muted-foreground text-sm">
+              No options found
+            </span>
+          </CommandEmpty>
+          {options.map((option) => (
+            <CommandItem
+              key={option.id}
+              onSelect={() => {
+                onSelect(option.value);
+                setOpen(false);
+              }}
+            >
+              {option.children}
+            </CommandItem>
+          ))}
+        </CommandList>
+      </CommandResponsiveDialog>
+    </>
+  );
+};
---
+++ makai/src/modules/meetings/ui/view/components/meeting-form.tsx
@@ -0,0 +1,199 @@
+import { useTRPC } from "@/trpc/client";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  meetingsUpdateSchema,
+  meetingsCreateSchema,
+} from "@/modules/meetings/schemas";
+import { MeetingsGetOne } from "@/modules/meetings/types";
+import { MAX_PAGE_SIZE } from "@/constants";
+import { CommandSelect } from "@/components/command-select";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { useState } from "react";
+import { CreateAgentDialog } from "@/modules/agents/ui/components/create-agent-dialog";
+
+interface MeetingFormProps {
+  onSuccess?: (id?: string) => void;
+  onCancel?: () => void;
+  initialValues?: MeetingsGetOne;
+}
+
+export const MeetingForm = ({
+  onSuccess,
+  onCancel,
+  initialValues,
+}: MeetingFormProps) => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const [agentSearch, setAgentSearch] = useState<string>("");
+  const [openCreateAgentDialog, setOpenCreateAgentDialog] =
+    useState<boolean>(false);
+
+  const agents = useQuery(
+    trpc.agents.getMany.queryOptions({
+      pageSize: MAX_PAGE_SIZE,
+      search: agentSearch,
+    }),
+  );
+
+  const createMeeting = useMutation(
+    trpc.meetings.create.mutationOptions({
+      onSuccess: async (data) => {
+        await queryClient.invalidateQueries(
+          trpc.meetings.getMany.queryOptions({}),
+        );
+        onSuccess?.(data.id);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    }),
+  );
+  const updateMeeting = useMutation(
+    trpc.meetings.update.mutationOptions({
+      onSuccess: async (data) => {
+        await queryClient.invalidateQueries(
+          trpc.meetings.getMany.queryOptions({}),
+        );
+        if (initialValues?.id) {
+          await queryClient.invalidateQueries(
+            trpc.meetings.getOne.queryOptions({ id: initialValues.id }),
+          );
+        }
+        onSuccess?.(data.id);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    }),
+  );
+
+  const form = useForm<z.infer<typeof meetingsCreateSchema>>({
+    resolver: zodResolver(meetingsCreateSchema),
+    defaultValues: {
+      name: initialValues?.name ?? "",
+      agentId: initialValues?.agentId ?? "",
+    },
+  });
+
+  const isEdit = !!initialValues?.id;
+  const isPending = createMeeting.isPending || updateMeeting.isPending;
+
+  const onSubmit = (values: z.infer<typeof meetingsCreateSchema>) => {
+    if (isEdit) {
+      updateMeeting.mutate({
+        ...values,
+        id: initialValues?.id,
+      });
+    } else {
+      createMeeting.mutate(values);
+    }
+  };
+
+  return (
+    <>
+      <CreateAgentDialog
+        open={openCreateAgentDialog}
+        onOpenChange={setOpenCreateAgentDialog}
+      />
+      <Form {...form}>
+        <form className="space-y-4 " onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="How to steal Ramen from Ichiraku?"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="agentId"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Agent</FormLabel>
+                <FormControl>
+                  <CommandSelect
+                    options={(agents.data?.items ?? []).map((agent) => ({
+                      label: agent.name,
+                      value: agent.id,
+                      children: (
+                        <div className="flex  items-center gap-x-2">
+                          <GeneratedAvatar
+                            seed={agent.name}
+                            variant="botttsNeutral"
+                            className="border size-6"
+                          />
+                          <span className="text-sm">{agent.name}</span>
+                        </div>
+                      ),
+                    }))}
+                    value={field.value}
+                    onSelect={(value) => form.setValue("agentId", value)}
+                    onSearch={setAgentSearch}
+                    placeholder="Select an agent"
+                  />
+                </FormControl>
+                <FormDescription>
+                  Not found the agent you&apos;re looking for?{" "}
+                  <button
+                    type="button"
+                    className="text-primary hover:underline"
+                    onClick={() => setOpenCreateAgentDialog(true)}
+                  >
+                    Create a new agent
+                  </button>
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex  justify-between items-center">
+            {onCancel && (
+              <Button
+                variant={"ghost"}
+                disabled={isPending}
+                type="button"
+                onClick={() => onCancel()}
+              >
+                Cancel
+              </Button>
+            )}
+            <Button
+              disabled={isPending}
+              type="submit"
+              onClick={form.handleSubmit(onSubmit)}
+            >
+              {isEdit ? "Update" : "Create"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
